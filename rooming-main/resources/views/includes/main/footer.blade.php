a42c49673451490627fad7122a7a891e905af667
yanyan <yanyannaylaa383@gmail.com>
2024-07-09 20:24:06 +0700

    first commit

---
+++ rooming-main/resources/views/includes/main/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+  <div class="footer-left">
+    &copy; ROOMING 2024 - Present by <a href="#" target="_blank">SCORPAPRANEDYA</a> <div class="bullet"></div> Powered by Stisla 2.3.0 &copy; 2024 by <a href="#" target="_blank">SCORPAPRANEDYA</a>
+  </div>
+  <div class="footer-right">
+    1.0
+  </div>
+</footer>
